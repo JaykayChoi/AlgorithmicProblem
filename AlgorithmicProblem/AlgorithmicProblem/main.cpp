ed1b38191c280314f37498546a10a3de17ffdff8
최정규 <jaykay@xlgames.com>
2017-09-12 17:05:54 +0900

    no message

---
+++ AlgorithmicProblem/AlgorithmicProblem/main.cpp
@@ -0,0 +1,18 @@
+#pragma warning (disable:4996)
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+//https://www.acmicpc.net/problem/6236
+
+//2805
+int main()
+{
+
+
+	return 0;
+}
